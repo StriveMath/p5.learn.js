f179687605d56a1e710774aec53b9c9f6dd0efdd
Maxim Schoemaker <maxim.schoemaker@gmail.com>
2024-01-08 09:55:57 +0100

    formatting

--- tests/js/filterCanvas/sketch.js
+++ tests/js/filterCanvas/sketch.js
@@ -1,12 +1,14 @@
-
 function setup() {
-  createCanvas(400, 400)
-  loadImage("https://lgobvciqjnxkpfghvptz.supabase.co/storage/v1/object/public/assets/global/bird.png",'fly')
+  createCanvas(400, 400);
+  loadImage(
+    "https://lgobvciqjnxkpfghvptz.supabase.co/storage/v1/object/public/assets/global/bird.png",
+    "fly"
+  );
 }
 
 function draw() {
-  background('black')
-  image(assets['fly'],width/2,height/2,200,200)
-  filterCanvas(INVERT)
-  drawTickAxes()
+  background("black");
+  image(assets["fly"], width / 2, height / 2, 200, 200);
+  filterCanvas(INVERT);
+  drawTickAxes();
 }
